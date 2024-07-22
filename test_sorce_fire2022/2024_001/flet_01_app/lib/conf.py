83804053980401f05c7e649e5e5a3f10144a8ca4
tossy0130 <tokotoko33ok@gmail.com>
2024-07-22 18:23:45 +0900

    2024 python flet 追加 0722 002

---
+++ test_sorce_fire2022/2024_001/flet_01_app/lib/conf.py
@@ -0,0 +1,21 @@
+class Config_Setting():
+    # ディレクトリ設定
+    LOG_DIR = "/Python_ソース比較_2022/test_sorce_fire2022/2024_001/LOG/"
+
+    # 原本（格納ディレクトリ）
+    ORIGINAL_DIR = "/Python_ソース比較_2022/test_sorce_fire2022/2024_001/ORIGINAL_DIR/"
+    # 比較用（格納ディレクトリ）
+    COMPARISON = "/Python_ソース比較_2022/test_sorce_fire2022/2024_001/COMPARISON/"
+
+    # 対象 URL
+    READ_URL = "https://192.168.254.204/kdemo/index.php"
+
+    @staticmethod
+    def get_config():
+        """
+        設定を辞書形式で取得する
+        """
+        return {
+            "LOG_DIR": Config_Setting.LOG_DIR,
+            "READ_URL": Config_Setting.READ_URL
+        }
--- test_sorce_fire2022/2024_001/flet_01_app/main.py
+++ test_sorce_fire2022/2024_001/flet_01_app/main.py
@@ -1,6 +1,16 @@
 import flet as ft
 import datetime
 
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# 外部ファイルから関数をインポート
+from lib.html_cut import HTML_CUT  # HTML 加工用
+from lib.conf import Config_Setting  # 設定情報
+from lib.file_related import File_related  # ファイル操作関係
+from lib.scraping import Scraping_Requests  # スクレイピング、クローリング用
+
 
 def main(page: ft.page):
     page.title = "ファイル改ざん検知 アプリ"
@@ -10,9 +20,43 @@ def main(page: ft.page):
     page.window_resizable = False  # ウィンドウサイズ変更可否
 
     url = ft.TextField(label="対象URL")  # ラベル
-    start_btn = ft.ElevatedButton("開始", icon="gavel")  # ボタン
+ #   start_btn = ft.ElevatedButton(
+ #       "開始", icon="gavel", on_click=start_btn_click)  # ボタン
 
-    page.add(url, start_btn)
+    def start_btn_click(e):
+        # =============== 初期設定
+        log_dir = Config_Setting.LOG_DIR  # ログ用ディレクトリ 作成
+        File_related.Check_Dir(log_dir)
+
+        original_dir = Config_Setting.ORIGINAL_DIR  # 原本用 ディレクトリ 作成
+        File_related.Check_Dir(original_dir)
+
+        comparison_dir = Config_Setting.COMPARISON  # 比較用 ディレクトリ 作成
+        File_related.Check_Dir(comparison_dir)
+
+        # =================================================================
+        # ===================== chromeドライバーの読み込み ==================
+        # =================================================================
+        # 読み込み対象　URL
+        url = Config_Setting.READ_URL
+
+        driver = webdriver.Chrome()
+        driver.get(url)
+
+        # Scraping_Requests クラスオブジェクト生成
+
+        # html_text = driver.page_source  # selenium
+
+        scraper = Scraping_Requests()
+        scraper.GET_Scraping_Requests(
+            'index.txt', driver.page_source, original_dir, comparison_dir)
+
+    # ========================================= END start_btn_click
+
+    # ================ コンポーネント追加
+    page.add(url,
+             ft.ElevatedButton("開始", icon="gavel", on_click=start_btn_click)
+             )
 
 
 ft.app(target=main)
